b15cdd4008d7f8ef17a04185869f799a34fb7d3f
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-06-30 09:09:06 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - for entry
+ * Return: 0 if exited properly , not zero otherwise
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
