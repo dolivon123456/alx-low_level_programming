5e71753936e64644a90a6ce84c6c0b2454bc4039
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-05 13:35:01 -0700

    push

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: Always 0.
+ * jack_bauer - every minute of the day of jack bauer
  */
-int main(void)
+void jack_bauer(void)
 {
-	times_table();
-	return (0);
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
