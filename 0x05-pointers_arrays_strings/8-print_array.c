0cf4f252d2d18815058204f73764dcdd1b64140c
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-12 08:32:36 -0700

    push

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,24 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_array- print array
- *@a: array
- *@n: array size
+ * print_array - prints the items in an array
+ * @arr: array to be printed
+ * @n: number of element to print
  *
+ * Created by: Sanctus-Peter
+ * cc: 12th, july 2022.
  */
 
-void print_array(int *a, int n)
+void print_array(int *arr, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n - 1; i++)
-		{
-			printf("%d, ", a[i]);
-		}
-		printf("%d\n", a[i]);
+		printf("%d", arr[i]);
+		if (i < n - 1)
+			printf(", ");
 	}
-}        
+	putchar('\n');
+} 
