a6e3e4248c942ddb41e4682719a3b977ffbc8766
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-14 05:46:03 -0700

    push

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,29 +8,28 @@
  * @n: int
  *
  * Return: dest
+ *
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+int j = 0;
+int i = 0;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++
-	}
-
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		if (j < n)
-		{
-			dest[j + i] = src[j];
-		}
-		else
-		{
-			dest[j + i] = '\0';
-		}
-		return (dest);
-	}
+while (dest[i] != '\0')
+{
+i++;
 }
+
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < n)
+{
+dest[j + i] = src[j];
+}
+else
+
+dest[j + i] = '\0';
+}
+return (dest);
+}
+
