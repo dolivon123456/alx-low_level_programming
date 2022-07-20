36714854c44ca458d16857c9acd05877ea68e2c2
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-20 05:04:26 -0700

    push

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number
+ *
+ * @n: number
+ *
+ * Return: factorial of a number
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
