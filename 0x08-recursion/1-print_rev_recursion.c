2b68a0d6223a059438b33e96dc831b3292b0e13c
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-20 04:35:34 -0700

    push

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _print_rev_recursion - prints a string in reverse
@@ -12,7 +11,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 	_print_rev_recursion(s + 1);
