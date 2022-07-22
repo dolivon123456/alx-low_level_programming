01b0e0cb8d42258bad70566e559e6da3807f556b
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-07-22 07:54:12 -0700

    push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", arg - 1);
+	return (0);
+}
