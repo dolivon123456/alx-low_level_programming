937f6ee58a7d61bfcd900ab27da349b4b10c26c4
Olatunji Adedolapo Sharon <dolivon555@gmail.com>
2022-08-01 09:45:09 -0700

    push

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,83 @@
+#include "dog.h"
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ *new_dog - create a new dog
+ *@name: dog name char
+ *@age: dog age flaot
+ *@owner: dog owner char
+ *
+ * Return: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+
+	doggy->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggy->name == NULL)
+	{
+		free(doggy);
+		return (NULL);
+	}
+
+	doggy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
+
+	doggy->name = _strcopy(doggy->name, name);
+	doggy->age = age;
+	doggy->owner = _strcopy(doggy->owner, owner);
+
+	return (doggy);
+}
+
+/**
+ *_strlen - finds the length of a string.
+ *@str: the string to be measured
+ *
+ *Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ *_strcopy - copies a string pointed to by src, includeing
+ *		the terminating null byte, to a buffer pointed to bt dest.
+ *
+ *@dest: the buffer storing the copy of the string
+ *@src: the source string
+ *
+ *Return: The new struct dog.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *free_dog - free memory created with malloc
+ *@d: memory to free
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
