d930ea474f20af90a045e056112a125a28b5db2f
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-08-18 13:02:11 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,53 +1,16 @@
 #include "main.h"
-
 /**
- * dec2bin - convert dec to binary
- * @n: digit to be printed in binary form
- * @buffer: buffer to hold the converted decimal to binary
+ * print_binary - print binary representation on a number
+ * @n: integer
+ *
+ * Return: nothing
  */
-
-void dec2bin(unsigned long int n, char *buffer)
-{
-	char res[32];
-	int i = 0;
-
-	while (n)
-	{
-		if (n & 1)
-			res[i++] = '1';
-		else
-			res[i++] = '0';
-		n >>= 1;
-	}
-	res[i--] = '\0';
-	while (res[i] && i >= 0)
-	{
-		*buffer++ = res[i];
-		i--;
-	}
-	*buffer = '\0';
-}
-
-/**
- * print_binary - print binary
- * @n: decimal digit to be converted to binary
- */
-
 void print_binary(unsigned long int n)
 {
-	char buf[32];
-	int i = 0;
+	unsigned long int bin;
 
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-
-	dec2bin(n, buf);
-
-	while (buf[i])
-	{
-		putchar(buf[i++]);
-	}
+	if (n > 1)
+	print_binary(n >> 1);
+	bin = n & 1;
+	_putchar(bin + '0');
 }
