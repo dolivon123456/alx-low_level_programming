b1e8f9b158dad371f4db7cd218d01672c7607564
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-08-22 12:21:45 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+int fd, rstatus, i;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
+if (filename == NULL)
 	return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+if (fd == -1)
+	return (-1);
+
+if (text_content)
+{
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	rstatus = write(fd, text_content, i);
+	if (rstatus == -1)
+		return (-1);
+}
+
+close(fd);
+return (1);
 }
