f993f55c212b93f98345e2bf8f93445d1dae6c33
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-10-04 00:46:36 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/dolivon123456/alx-low_level_programming/master/0x18-dynamic_libraries/libeleccrazy.so
-export LD_PRELOAD=/tmp/libeleccrazy.so
+wget -P /tmp https://github.com/dolivon123456/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
